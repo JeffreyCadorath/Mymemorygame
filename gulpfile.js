2e47e72501773b22a7643dccf6a4d87887d55528
Jeff Cadorath <JJcadorath@hotmail.com>
2019-05-13 10:49:24 -0500

    added clean-css to minify CSS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
  
-gulp.task('autoprefixing', () =>
+gulp.task('styles', () =>
     gulp.src('./src/**/*.css')
+    .pipe(cleanCSS())
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
         .pipe(gulp.dest('./dest/css'))
 );
+
