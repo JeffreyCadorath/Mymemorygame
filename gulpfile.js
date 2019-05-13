ca95ffbf1ae821fdd1b3c47650c41d73afbc608e
Jeff Cadorath <JJcadorath@hotmail.com>
2019-05-13 10:56:06 -0500

    Updated imagemin to minify all PNG images

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 let cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
  
-gulp.task('styles', () =>
-    gulp.src('./src/**/*.css')
+gulp.task('styles', () => {
+  return gulp.src('./src/**/*.css')
     .pipe(cleanCSS())
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('./dest/css'))
-);
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./dest/css'))
+});
+
+gulp.task('imageMin', () => {
+  return gulp.src('./src/images/**/*.png')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dest/images'));
+});
 
